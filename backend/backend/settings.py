fbcab0a82aa7d2f1f43dfe30388b0583fda9f768
Jeremy Chen <jeremy886@gmail.com>
2018-06-27 14:47:55 +1000

    setup graphene

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'graphene_django',
     'simple_app',
 ]
 
@@ -119,3 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# GRAPHENE
+
+GRAPHENE = {
+    'SCHEMA': 'backend.schema.schema',
+}
